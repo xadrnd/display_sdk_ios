7a7fe7c7f93bf37b9895950082384adf82eec02d
Jacob Zelek <jacob.zelek@xad.com>
2017-12-05 15:55:27 -0800

    Fix for XMLUtil path

--- XADDisplaySdk/XADDisplaySdk/XADDisplaySdk.h
+++ XADDisplaySdk/XADDisplaySdk/XADDisplaySdk.h
@@ -13,7 +13,3 @@ FOUNDATION_EXPORT double XADDisplaySdkVersionNumber;
 
 //! Project version string for XADDisplaySdk.  BUG no string
 FOUNDATION_EXPORT const unsigned char XADDisplaySdkVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <XADDisplaySdk/PublicHeader.h>
-
-#import "vast/XADVASTXMLUtil.h"
